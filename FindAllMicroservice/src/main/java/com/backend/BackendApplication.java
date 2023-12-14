2bdfa12720a8120e1847843227fe05bbc74a5505
Lucas Bottrel Lopes de Moura <lucasbottrel03@gmail.com>
2023-12-14 18:16:21 -0300

    Separação de serviços em microserviços

--- FindAllMicroservice/src/main/java/com/backend/BackendApplication.java
+++ FindAllMicroservice/src/main/java/com/backend/BackendApplication.java
@@ -5,19 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 
 @SpringBootApplication
 public class BackendApplication {
-
-	public static final String topicExchangeName = "spring-boot-exchange";
-	public static final String queueName = "spring-boot";
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
@@ -32,28 +22,5 @@ public class BackendApplication {
 						.allowedHeaders("*");
 			}
 		};
-	}
-
-	@Bean
-	Queue queue() {
-		return new Queue(queueName, false);
-	}
-
-	@Bean
-	TopicExchange exchange() {
-		return new TopicExchange(topicExchangeName);
-	}
-
-	@Bean
-	Binding binding(Queue queue, TopicExchange exchange) {
-		return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
-	}
-
-	@Bean
-	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory) {
-		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-		container.setConnectionFactory(connectionFactory);
-		container.setQueueNames(queueName);
-		return container;
 	}
 }
